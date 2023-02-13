8dbe02594d3cbffeda8acc2a88264fadd4e9634a
DESKTOP-VIAM49V\PLENG <hathairatsuthon057@gmail.com>
2023-02-13 11:09:43 +0700

    สร้าง Function ตัวเลข 0-9 ขึ้นมาเอง

---
+++ number.py
@@ -0,0 +1,11 @@
+import sys
+# sys.stdout.write("Hello World")
+
+def atd_command(text):
+    sys.stdout.write(text + '\n')
+
+atd_command("mother fucker")
+
+for i  in range(0,10):
+ double = i * 2
+ print(i)
